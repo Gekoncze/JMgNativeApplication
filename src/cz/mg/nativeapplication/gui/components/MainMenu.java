26fb6f2602977256e0447b4deb2043607c810d0e
Me <gekoncze@seznam.cz>
2021-07-11 15:32:16 +0200

    added tab close button + keyboard shortcut

--- src/cz/mg/nativeapplication/gui/components/MainMenu.java
+++ src/cz/mg/nativeapplication/gui/components/MainMenu.java
@@ -1,5 +1,7 @@
 package cz.mg.nativeapplication.gui.components;
 
+import cz.mg.annotations.classes.Utility;
+import cz.mg.annotations.requirement.Mandatory;
 import cz.mg.nativeapplication.gui.MainWindow;
 import cz.mg.nativeapplication.gui.handlers.ActionUserEventHandler;
 
@@ -9,8 +11,13 @@ import static java.awt.event.InputEvent.CTRL_DOWN_MASK;
 import static java.awt.event.InputEvent.SHIFT_DOWN_MASK;
 
 
-public class MainMenu extends JMenuBar {
-    public MainMenu(MainWindow mainWindow) {
+public @Utility class MainMenu extends JMenuBar {
+    public MainMenu(@Mandatory MainWindow mainWindow) {
+        add(createFileMenu(mainWindow));
+        add(createViewMenu(mainWindow));
+    }
+
+    private JMenu createFileMenu(@Mandatory MainWindow mainWindow){
         JMenuItem newProjectMenuItem = new JMenuItem("New project");
         newProjectMenuItem.setMnemonic('N');
         newProjectMenuItem.setAccelerator(KeyStroke.getKeyStroke('N', CTRL_DOWN_MASK));
@@ -39,15 +46,27 @@ public class MainMenu extends JMenuBar {
         exitMenuItem.setMnemonic('E');
         exitMenuItem.addActionListener(new ActionUserEventHandler(mainWindow, mainWindow::exit));
 
-        JMenu fileMenu = new JMenu("File");
-        fileMenu.setMnemonic('F');
-        fileMenu.add(newProjectMenuItem);
-        fileMenu.add(openProjectMenuItem);
-        fileMenu.add(saveProjectMenuItem);
-        fileMenu.add(saveProjectAsMenuItem);
-        fileMenu.add(closeProjectMenuItem);
-        fileMenu.add(exitMenuItem);
+        JMenu menu = new JMenu("File");
+        menu.setMnemonic('F');
+        menu.add(newProjectMenuItem);
+        menu.add(openProjectMenuItem);
+        menu.add(saveProjectMenuItem);
+        menu.add(saveProjectAsMenuItem);
+        menu.add(closeProjectMenuItem);
+        menu.add(exitMenuItem);
+        return menu;
+    }
 
-        add(fileMenu);
+    private JMenu createViewMenu(@Mandatory MainWindow mainWindow){
+        JMenuItem closeActiveTab = new JMenuItem("Close active tab");
+        closeActiveTab.setAccelerator(KeyStroke.getKeyStroke('W', CTRL_DOWN_MASK));
+        closeActiveTab.addActionListener(new ActionUserEventHandler(mainWindow,
+            () -> mainWindow.getMainView().getMainTabView().closeActiveTab()
+        ));
+
+        JMenu menu = new JMenu("View");
+        menu.setMnemonic('V');
+        menu.add(closeActiveTab);
+        return menu;
     }
 }
--- src/cz/mg/nativeapplication/gui/components/MainTabView.java
+++ src/cz/mg/nativeapplication/gui/components/MainTabView.java
@@ -6,8 +6,11 @@ import cz.mg.annotations.storage.Link;
 import cz.mg.nativeapplication.entities.mg.MgProject;
 import cz.mg.nativeapplication.gui.MainWindow;
 import cz.mg.nativeapplication.gui.components.object.ProjectView;
+import cz.mg.nativeapplication.gui.handlers.ActionUserEventHandler;
+import cz.mg.nativeapplication.gui.utilities.GridBagConstraintFactory;
 
 import javax.swing.*;
+import java.awt.*;
 
 import static cz.mg.nativeapplication.gui.utilities.NavigationCache.Node;
 
@@ -22,14 +25,48 @@ public class MainTabView extends JTabbedPane {
     public void open(@Optional Node node){
         if(node != null){
             if(node.getSelf() instanceof MgProject){
-                addTab(
-                    node.getName(),
-                    node.getIcon(),
-                    new ProjectView(mainWindow, (MgProject) node.getSelf())
-                );
+                addTab(node, new ProjectView(mainWindow, (MgProject) node.getSelf()));
             }
 
             // todo - add support for more object types
         }
+    }
+
+    public void closeActiveTab(){
+        int index = getSelectedIndex();
+        if(index != -1){
+            remove(index);
+        }
+    }
+
+    private void addTab(@Mandatory Node node, @Mandatory Component component){
+        addTab(null, null, component);
+        setTabComponentAt(getTabCount() - 1, createTabHeader(node, component));
+    }
+
+    private JPanel createTabHeader(@Mandatory Node node, @Mandatory Component component){
+        JPanel header = new JPanel();
+        header.setLayout(new GridBagLayout());
+        header.setOpaque(false);
+
+        JLabel label = new JLabel();
+        label.setText(node.getName());
+        label.setIcon(node.getIcon());
+        label.setOpaque(false);
+        header.add(label, new GridBagConstraintFactory().create(
+            0, 0, 0, 0, 0, 0, 0, 8
+        ));
+
+        JButton closeButton = new JButton("x");
+        closeButton.setBorder(null);
+        closeButton.setOpaque(false);
+        closeButton.setBackground(new Color(0, 0, 0, 0));
+        closeButton.setForeground(new Color(180, 180, 180, 255));
+        closeButton.addActionListener(new ActionUserEventHandler(mainWindow, () -> remove(component)));
+        header.add(closeButton, new GridBagConstraintFactory().create(
+            1, 0, 0, 0, 0, 0, 0, 0
+        ));
+
+        return header;
     }
 }
