4fe2592e1445f0f4647d4d9040da86baa73cd80b
Me <gekoncze@seznam.cz>
2021-09-28 16:12:18 +0200

    added test shared to delete service test

--- test/cz/mg/nativeapplication/DeleteServiceTest.java
+++ test/cz/mg/nativeapplication/DeleteServiceTest.java
@@ -20,8 +20,8 @@ public class DeleteServiceTest implements Test {
         new SingleTestRunner().run(new DeleteServiceTest());
     }
 
-    @TestCase
-    public void test(){
+    @TestCase(order = 0)
+    public void testDelete(){
         MgProject project = new MgProject();
         MgLocation root = new MgLocation();
         project.root = root;
@@ -59,5 +59,47 @@ public class DeleteServiceTest implements Test {
         assertNotContains(root.components, structure);
         assertNull(output.type);
         assertNull(expression.child);
+    }
+
+    @TestCase(order = 1)
+    public void testShared(){
+        MgProject project = new MgProject();
+        MgLocation root = new MgLocation();
+        project.root = root;
+        MgStructure structure = new MgStructure();
+        MgVariable field = new MgVariable();
+        structure.variables.addLast(field);
+        MgFunction function = new MgFunction();
+        MgVariable output = new MgVariable();
+        function.output = output;
+        output.type = structure;
+        root.components.addLast(structure);
+        root.components.addLast(structure); // imitating shared ownership
+        root.components.addLast(function);
+        MgExpressionCommand command = new MgExpressionCommand();
+        function.commands.addLast(command);
+        MgMemberExpression expression = new MgMemberExpression();
+        command.expression = expression;
+        expression.child = field;
+
+        assertSame(root, project.root);
+        assertContains(root.components, function);
+        assertSame(output, function.output);
+        assertContains(function.commands, command);
+        assertEquals(expression, command.expression);
+        assertContains(root.components, structure, 2);
+        assertSame(structure, output.type);
+        assertSame(field, expression.child);
+
+        new DeleteService().remove(project, root.components, 0);
+
+        assertSame(root, project.root);
+        assertContains(root.components, function);
+        assertSame(output, function.output);
+        assertContains(function.commands, command);
+        assertEquals(expression, command.expression);
+        assertContains(root.components, structure, 1);
+        assertSame(structure, output.type);
+        assertSame(field, expression.child);
     }
 }
