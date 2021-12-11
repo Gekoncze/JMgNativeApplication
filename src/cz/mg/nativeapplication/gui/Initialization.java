1ed62b033af8cc4f3a0e3a8f51e5b2107ff6e9e2
Me <gekoncze@seznam.cz>
2021-12-11 15:08:38 +0100

    ???

--- src/cz/mg/nativeapplication/gui/Initialization.java
+++ src/cz/mg/nativeapplication/gui/Initialization.java
@@ -4,15 +4,14 @@ import cz.mg.annotations.classes.Utility;
 import cz.mg.annotations.requirement.Mandatory;
 import cz.mg.annotations.storage.Shared;
 import cz.mg.classdetector.ClassReader;
+import cz.mg.collections.list.List;
 import cz.mg.entity.EntityClassInitializer;
 import cz.mg.entity.mapper.MapperFactory;
-import cz.mg.nativeapplication.gui.services.ClassProvider;
 import cz.mg.nativeapplication.gui.services.ProjectMapperProvider;
 
 
 public @Utility class Initialization {
     private final @Mandatory @Shared ClassReader classReader = new ClassReader();
-    private final @Mandatory @Shared ClassProvider classProvider = new ClassProvider();
     private final @Mandatory @Shared EntityClassInitializer entityClassInitializer = new EntityClassInitializer();
     private final @Mandatory @Shared ProjectMapperProvider projectMapperProvider = new ProjectMapperProvider();
     private final @Mandatory @Shared MapperFactory mapperFactory = new MapperFactory();
@@ -21,20 +20,20 @@ public @Utility class Initialization {
     }
 
     public void init(){
-        initClasses();
-        initEntityClasses();
-        initObjectMappers();
+        List<Class> classes = loadClasses();
+        initEntityClasses(classes);
+        initObjectMappers(classes);
     }
 
-    private void initClasses(){
-        classProvider.set(classReader.read(Application.class.getResourceAsStream("Classes.txt")));
+    private @Mandatory List<Class> loadClasses(){
+        return classReader.read(Application.class.getResourceAsStream("Classes.txt"));
     }
 
-    private void initEntityClasses(){
-        entityClassInitializer.init(classProvider.get());
+    private void initEntityClasses(@Mandatory List<Class> classes){
+        entityClassInitializer.init(classes);
     }
 
-    private void initObjectMappers(){
-        projectMapperProvider.set(mapperFactory.create(classProvider.get()));
+    private void initObjectMappers(@Mandatory List<Class> classes){
+        projectMapperProvider.set(mapperFactory.create(classes));
     }
 }
